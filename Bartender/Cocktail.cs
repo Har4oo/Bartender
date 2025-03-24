5fd9aea7c2bd5ef09615aa349ad6b5ad0370ef55
Har4o <harihharalampiev@gmail.com>
2025-03-24 19:24:13 +0200

    Initial commit

---
+++ Bartender/Cocktail.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bartender
+{
+    internal class Cocktail
+    {
+
+        private string name;
+        private ArrayList ingredients;
+
+        public string Name { get => name; set => name = value; }
+        public ArrayList Ingredients { get => ingredients; set => ingredients = value; }
+
+        public Cocktail(string name, ArrayList ingredients)
+        {
+            this.Name = name;
+            this.Ingredients = ingredients;
+        }
+
+        public Cocktail()
+        {
+        }
+
+        public string assignCocktail()
+        {
+            StreamReader streamReader = new StreamReader("ten_random_cocktails.txt");
+
+            string line = streamReader.ReadLine();
+            string randomlyGivenCocktail = "";
+
+            if (!string.IsNullOrEmpty(line))
+            {
+                string[] cocktails = line.Split(new string[] { " | " }, StringSplitOptions.None);
+
+                Random rnd = new Random();
+
+                randomlyGivenCocktail = cocktails[rnd.Next(cocktails.Length)];
+            }
+            return randomlyGivenCocktail;
+        }
+    }
+}
